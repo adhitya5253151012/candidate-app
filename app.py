b49fc84f016210725efbe873ce57e470ad95878f
adhitya5253151012 <adhitya.5253151012@mhs.unimed.ac.id>
2026-05-29 22:32:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -109,19 +109,18 @@ left, right = st.columns([1,4])
 # ======================
 with left:
     st.markdown("""
-    <div class="sidebar-canva">
-        <h3>🏆 Sistem Kandidat</h3>
+<div class="sidebar-canva">
+    <h3>🏆 Sistem Kandidat</h3>
 
-        <div class="nav-item nav-active">📊 Dashboard</div>
-        <div class="nav-item">👥 Kandidat</div>
-        <div class="nav-item">📈 Analisis</div>
-        <div class="nav-item">🔗 Himpunan</div>
-        <div class="nav-item">🔀 Kombinasi</div>
-        <div class="nav-item">∑ Model</div>
-        <div class="nav-item">⚙️ Pengaturan</div>
-    </div>
-    """, unsafe_allow_html=True)
-
+    <div class="nav-item nav-active">📊 Dashboard</div>
+    <div class="nav-item">👥 Kandidat</div>
+    <div class="nav-item">📈 Analisis</div>
+    <div class="nav-item">🔗 Himpunan</div>
+    <div class="nav-item">🔀 Kombinasi</div>
+    <div class="nav-item">∑ Model</div>
+    <div class="nav-item">⚙️ Pengaturan</div>
+</div>
+""", unsafe_allow_html=True)
     st.write("")
 
     st.subheader("Input Kandidat")
